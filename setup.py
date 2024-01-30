3f26688914e312dbeda6a3fa3df669f8f008158a
Himanshu Arora <129079320+Himanshu-1703@users.noreply.github.com>
2024-01-30 17:37:05 +0530

    added dependencies to the setup.py file

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import find_packages, setup
 
+def get_requirements(requirements_file):
+    with open(requirements_file) as file:
+        requirements_list = file.readlines()
+        requirements_list = [req.replace('\n','') for req in requirements_list]
+        return requirements_list
+        
+
+
+
 setup(
     name='src',
     packages=find_packages(),
@@ -7,4 +16,5 @@ setup(
     description='Predict the trip duration of taxis in NY city',
     author='Himanshu Arora',
     license='MIT',
+    install_requires = get_requirements('requirements.txt')
 )
